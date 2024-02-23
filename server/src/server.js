fb26858cb4ca843bb24af548a8004f564a645af7
BauteNonSinger <bautista19298@gmail.com>
2024-02-23 14:06:02 +0100

    Planet data from csv

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 const app = require('./app')
+
+const {loadPlanetsData} = require('./models/planets.model');
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Preparado en el puerto ${PORT}`)
-})
+async function startServer() {
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+      console.log(`Escuchando en el puerto ${PORT}`);
+    });
+}
+
+startServer()
