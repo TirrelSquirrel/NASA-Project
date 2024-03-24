262ff0c66ecdd83b0e37ab792f697308daa9baf0
BauteNonSinger <bautista19298@gmail.com>
2024-03-24 09:53:02 +0100

    Added new functions to launches model

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,23 +1,38 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    ricket: 'Explorer',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true
+  flightNumber: 100,
+  mission: "Kepler Exploration X",              //Client
+  ricket: "Explorer",                           //Client
+  launchDate: new Date("December 27, 2030"),    //Client
+  destination: "Kepler-442 b",                  //Client
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
-    return Array.from(launches.values());
+  return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["ZeroToMastery", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
 
 module.exports = {
-    getAllLaunches,
-}
+  getAllLaunches,
+  addNewLaunch
+};
