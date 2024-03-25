6c245936a2be28efb4d2bf2efc8b7d1c4a2b9519
BauteNonSinger <bautista19298@gmail.com>
2024-03-25 14:57:01 +0100

    Added supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,24 +1,19 @@
-describe('Test GET / launches', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
-    });
-    
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+    .get("/launches")
+    .expect(200)
+    .expect('Content-Type', /json/);
+  });
 });
 
+describe("Test POST /launches", () => {
+  test("It should respond with 200 success", () => {});
 
-describe('Test POST / launches', () => {
-    test('It should respond with 200 success', () => {
-        
-    });
-    
-    test('It should catch missing required properties', () => {
-        
-    });
-    
+  test("It should catch missing required properties", () => {});
 
-    test('It should catch invalid dates', () => {
-        
-    });
-    
+  test("It should catch invalid dates", () => {});
 });
